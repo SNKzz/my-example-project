915a9eed4fee3221d8b0fa8b4b292aef501ea2e3
Hoàng Đô Anh <do@Mac-mini-cua-o.local>
2026-04-15 14:47:46 +0700

    Update 2 file HTML and JS for new example

--- click_button_text.js
+++ click_button_text.js
@@ -2,3 +2,22 @@ function clickButtonText(){
     const replacedText = "Hello World"
     document.getElementById("text").innerHTML = replacedText;
 }
+
+function clickButtonAlert(){
+    alert("Hello World");
+}
+
+function clickButtonConsole(){
+    console.log("console da log ra");
+}
+
+const textPointed = document.getElementById("textOnChange");
+textPointed.addEventListener("mouseover", overMouse) 
+textPointed.addEventListener("mouseout", outMouse) 
+function overMouse(){
+    document.getElementById("textOnChange").style.color = "red";
+}
+
+function outMouse(){
+    document.getElementById("textOnChange").style.color = "black";
+}
